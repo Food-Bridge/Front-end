a29698dda5333ddff43878ba67036c34b9dd1567
seon318 <jys7861@gmail.com>
2024-01-22 22:21:44 +0900

    Feat: Add Login components

---
+++ src/view/component/InputBox/InputBox.js
@@ -0,0 +1,8 @@
+import './InputBox.css'
+
+export default function InputBox() {
+  return (
+    <input className='inputBox' placeholder= '  아이디 또는 이메일'>
+    </input>
+  )
+}
---
+++ src/view/component/KakaoLogin/KakaoLogin.js
@@ -0,0 +1,13 @@
+import './KakaoLogin.css';
+import logo from '../../../data/KakaoLogo.png';
+
+export default function KakaoBox() {
+  return (
+    <button className='kakaoLogin'>
+      <div className='kakaoLogin-logo'>
+        <img src={logo} alt='Kakao Login' />
+      </div>
+      <p className='kakaoLogin-title'>카카오 로그인</p>
+    </button>
+  );
+}
