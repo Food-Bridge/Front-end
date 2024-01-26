afbc10841ab8652c9ed93d9e22508f949a522f68
seon318 <jys7861@gmail.com>
2024-01-26 18:21:49 +0900

    Feat: Add components and pages to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import './App.css';
-import MyList from './view/pages/MyListPage/MyList';
+import Category from './view/component/Category/Category.js';
+import Coupon from './view/component/Coupon/Coupon.js';
+import CouponDownloaded from './view/component/CouponDownloaded/CouponDownloaded.js';
+import GoogleBtn from './view/component/GoogleBtn/GoogleBtn.js';
+import InputBox from './view/component/InputBox/InputBox.js';
+import KakaoLogin from './view/component/KakaoLogin/KakaoLogin.js';
+import LogInBtn from './view/component/LogInBtn/LogInBtn.js';
+import LogoBar from './view/component/LogoBar/LogoBar.js';
+import MenuBar from './view/component/MenuBar/MenuBar.js';
+import MenuBlock from './view/component/MenuBlock/MenuBlock.js';
+import MyListBlock from './view/component/MyListBlock/MyListBlock.js';
+import MyListDeliver from './view/component/MyListDeliver/MyListDeliver.js';
+import MyListMain from './view/component/MyListMain/MyListMain.js';
+import PlusInfo from './view/component/PlusInfo/PlusInfo.js';
+import SearchBar from './view/component/SearchBar/SearchBar.js';
+import StoreDeliverTogo from './view/component/StoreDeliverTogo/StoreDeliverTogo.js';
 
-
+// pages
+import MyList from './view/pages/MyListPage/MyList.js';
+import SignIn from './view/pages/SignInPage/SignIn.js';
+import SignUp from './view/pages/SignUpPage/SignUp.js';
+import Store from './view/pages/StorePage/Store.js';
 function App() {
   return (
-    <div className="App">
-      <MyList />
+    <div className='App'>
+      {/*components*/}
+      {/* <Category />
+      <Coupon />
+      <CouponDownloaded />
+      <GoogleBtn />
+      <InputBox />
+      <KakaoLogin />
+      <LogInBtn />
+      <LogoBar />
+      <MenuBar />
+      <MenuBlock />
+      <MyListBlock />
+      <MyListDeliver />
+      <MyListMain />
+      <PlusInfo />
+      <SearchBar />
+      <StoreDeliverTogo /> */}
+
+      {/*pages*/}
+      {/* <MyList />
+      <SignIn />
+      <SignUp /> */}
+      <Store count={1}/>
     </div>
   );
 }
