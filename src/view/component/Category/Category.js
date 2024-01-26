243851b81cd5331c341ea1453f3fa4f4adff62e2
김선민 <sunmindev@gimseonmin-ui-MacBookAir.local>
2024-01-26 17:44:19 +0900

    Feat: Add Category Component

--- src/view/component/Category/Category.js
+++ src/view/component/Category/Category.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Category.scss'
 
-function Category({name}) {
+function Category({category}) {
   return (
     <div className='Category'>
         <header className='category-frame'>
             <div className='category-imgBlock'></div>
-            <h1 className='category-name'>{name}</h1>
+            <h1 className='category-name'>{category}</h1>
         </header>
     </div>
   )
