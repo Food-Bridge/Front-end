f976d729d01d76def2e7cf52d8c4936cccd9bfbb
seon318 <jys7861@gmail.com>
2024-01-27 12:30:54 +0900

    Feat: Add searchRank component

---
+++ src/view/component/SearchRank/SearchRank.js
@@ -0,0 +1,14 @@
+import './SearchRank.scss';
+
+import { RxTriangleUp, RxTriangleDown } from 'react-icons/rx';
+import { MdOutlineHorizontalRule } from 'react-icons/md';
+
+export default function SearchRank({ rank, text, type }) {
+  return (
+    <div className='searchRank'>
+      <p className='searchRank-num'>{rank}</p>
+      <p className='searchRank-text'>{text}</p>
+      <div className='searchRank-icon'>{type === 'up' ? <RxTriangleUp color='red' size='24'/> : (type === 'down' ? <RxTriangleDown color='blue' size='24'/> : <MdOutlineHorizontalRule size='20'/>)}</div>
+    </div>
+  );
+}
