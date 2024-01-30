4af10fc42813447c36ae4d7d54237ba75f54c9a1
seon318 <jys7861@gmail.com>
2024-01-30 20:56:07 +0900

    Feat: Link pages

--- src/App.js
+++ src/App.js
@@ -1,58 +1,33 @@
-import Category from './view/components/Category/Category.js';
-import Coupon from './view/components/Coupon/Coupon.js';
-import GoogleBtn from './view/components/GoogleBtn/GoogleBtn.js';
-import InputBox from './view/components/InputBox/InputBox.js';
-import KakaoLogin from './view/components/KakaoLogin/KakaoLogin.js';
-import LogInBtn from './view/components/LogInBtn/LogInBtn.js';
-import LogoBar from './view/components/LogoBar/LogoBar.js';
-import MenuBar from './view/components/MenuBar/MenuBar.js';
-import MenuBlock from './view/components/MenuBlock/MenuBlock.js';
-import MyListBlock from './view/components/MyListBlock/MyListBlock.js';
-import MyListDeliver from './view/components/MyListDeliver/MyListDeliver.js';
-import MyListMain from './view/components/MyListMain/MyListMain.js';
-import PaymentMenu from './view/components/PaymentMenu/PaymentMenu.js';
-import PlusInfo from './view/components/PlusInfo/PlusInfo.js';
-import SearchBar from './view/components/SearchBar/SearchBar.js';
-import SearchRank from './view/components/SearchRank/SearchRank.js';
-import SearchRecord from './view/components/SearchRecord/SearchRecord.js';
-import StoreDeliverTogo from './view/components/StoreDeliverTogo/StoreDeliverTogo.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.scss'
 
-// pages
-import MyList from './view/pages/MyListPage/MyList.js';
-import SignIn from './view/pages/SignInPage/SignIn.js';
-import SignUp from './view/pages/SignUpPage/SignUp.js';
-import Store from './view/pages/StorePage/Store.js';
+import CouponList from './view/pages/CouponListPage/CouponList'
+import LogoBar from './view/components/LogoBar/LogoBar';
+import MainPage from './view/pages/MainPage/MainPage';
+import MyList from './view/pages/MyListPage/MyList';
+import Store from './view/pages/StorePage/Store'
+import StoreList from './view/pages/StoreListPage/StoreList'
+import SignIn from './view/pages/SignInPage/SignIn';
+import SignUp from './view/pages/SignUpPage/SignUp';
+
 
 function App() {
   return (
     <div className='App'>
-      {/*components*/}
-      {/* <Category />
-      <Coupon />
-      <CouponDownloaded />
-      <GoogleBtn />
-      <InputBox />
-      <KakaoLogin />
-      <LogInBtn />
-      <LogoBar />
-      <MenuBar />
-      <MenuBlock />
-      <MyListBlock />
-      <MyListDeliver />
-      <MyListMain />
-      <PlusInfo />
-      <SearchBar />
-      <StoreDeliverTogo /> */}
-      <PaymentMenu />
-      <SearchRank rank='1' text='그릭요거트' type='up' />
-      <SearchRecord text='치킨' />
-      <Coupon />
+      <Router>
+        <LogoBar />
+        <Routes>
+          <Route path='/' element={<MainPage />} />
 
-      {/*pages*/}
-      {/* <MyList />
-      <SignIn />
-      <SignUp /> 
-      <Store count={1}/>*/}
+          <Route path='users/signin/' element={<SignIn />} />
+          <Route path='users/signup/' element={<SignUp />} />
+
+          <Route path='user/' element={<MyList />} />
+          <Route path='user/coupon/' element={<CouponList />} />
+
+          <Route path='store/:store_id/' element={<Store />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/view/components/LogoBar/LogoBar.js
+++ src/view/components/LogoBar/LogoBar.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react';
-import { AiOutlineHome } from "react-icons/ai";
-import { LuUserCircle2 } from "react-icons/lu";
-import "./LogoBar.scss";
+import { AiOutlineHome } from 'react-icons/ai';
+import { LuUserCircle2 } from 'react-icons/lu';
+import './LogoBar.scss';
+import { useNavigate } from 'react-router-dom';
 
-class LogoBar extends Component {
-    render() {
-        return (
-            <div className='LogoBar'>
-                <header className='logoBar-frame'>
-                    <div className='logoBar-margin'>
-                        <div className='logoBar-home'><AiOutlineHome className='logoBar-homeIcon'/></div>
-                        <div className='logoBar-logo'><h1 className='logoBar-logoText'>LOGO</h1></div>
-                        <div className='logoBar-user'><LuUserCircle2 className='logoBar-userIcon'/></div>
-                    </div>
-                </header>
-            </div>
-        )
-    }
+export default function LogoBar() {
+  const navigate = useNavigate();
 
+  const handleHomeClick = () => {
+    navigate('/');
+  };
+
+  const handleUserClick = () => {
+    navigate('/users/signin');
+  };
+  return (
+    <div className='LogoBar'>
+      <header className='logoBar-frame'>
+        <div className='logoBar-margin'>
+          <button className='logoBar-home' onClick={handleHomeClick}>
+            <AiOutlineHome className='logoBar-homeIcon' />
+          </button>
+          <div className='logoBar-logo'>
+            <h1 className='logoBar-logoText'>LOGO</h1>
+          </div>
+          <button className='logoBar-user' onClick={handleUserClick}>
+            <LuUserCircle2 className='logoBar-userIcon' />
+          </button>
+        </div>
+      </header>
+    </div>
+  );
 }
-
-export default LogoBar;
