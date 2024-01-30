45b8a13d3367f51ad19124ba88058b5877c05c3b
김선민 <sunmindev@gimseonmin-ui-MacBookAir.local>
2024-01-30 23:08:22 +0900

    Feat: Add App Router

--- src/App.js
+++ src/App.js
@@ -1,58 +1,19 @@
-import Category from './view/components/Category/Category.js';
-import Coupon from './view/components/Coupon/Coupon.js';
-import GoogleBtn from './view/components/GoogleBtn/GoogleBtn.js';
-import InputBox from './view/components/InputBox/InputBox.js';
-import KakaoLogin from './view/components/KakaoLogin/KakaoLogin.js';
-import LogInBtn from './view/components/LogInBtn/LogInBtn.js';
-import LogoBar from './view/components/LogoBar/LogoBar.js';
-import MenuBar from './view/components/MenuBar/MenuBar.js';
-import MenuBlock from './view/components/MenuBlock/MenuBlock.js';
-import MyListBlock from './view/components/MyListBlock/MyListBlock.js';
-import MyListDeliver from './view/components/MyListDeliver/MyListDeliver.js';
-import MyListMain from './view/components/MyListMain/MyListMain.js';
-import PaymentMenu from './view/components/PaymentMenu/PaymentMenu.js';
-import PlusInfo from './view/components/PlusInfo/PlusInfo.js';
-import SearchBar from './view/components/SearchBar/SearchBar.js';
-import SearchRank from './view/components/SearchRank/SearchRank.js';
-import SearchRecord from './view/components/SearchRecord/SearchRecord.js';
-import StoreDeliverTogo from './view/components/StoreDeliverTogo/StoreDeliverTogo.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-// pages
-import MyList from './view/pages/MyListPage/MyList.js';
-import SignIn from './view/pages/SignInPage/SignIn.js';
-import SignUp from './view/pages/SignUpPage/SignUp.js';
-import Store from './view/pages/StorePage/Store.js';
+// import MainPage from './view/pages/MainPage/MainPage';
+import SignIn from './view/Pages/SignInPage/SignIn'
+import SignUp from './view/Pages/SignUpPage/SignUp'
 
 function App() {
   return (
     <div className='App'>
-      {/*components*/}
-      {/* <Category />
-      <Coupon />
-      <CouponDownloaded />
-      <GoogleBtn />
-      <InputBox />
-      <KakaoLogin />
-      <LogInBtn />
-      <LogoBar />
-      <MenuBar />
-      <MenuBlock />
-      <MyListBlock />
-      <MyListDeliver />
-      <MyListMain />
-      <PlusInfo />
-      <SearchBar />
-      <StoreDeliverTogo /> */}
-      <PaymentMenu />
-      <SearchRank rank='1' text='그릭요거트' type='up' />
-      <SearchRecord text='치킨' />
-      <Coupon />
-
-      {/*pages*/}
-      {/* <MyList />
-      <SignIn />
-      <SignUp /> 
-      <Store count={1}/>*/}
+      <Router>
+        <Routes>
+          <Route path='/' element={<SignUp />} />
+          <Route path='/logIn/' element={<SignIn />} />
+          <Route path='/signup/' element={<SignUp />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
