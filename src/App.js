ea8e3e8a35399f655517bdd67c7bbce7fbb5e0cc
김선민 <sunmindev@gimseonmin-ui-MacBookAir.local>
2024-01-30 23:31:13 +0900

    Delete App

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import LogoBar from './view/components/LogoBar/LogoBar';
 import MainPage from './view/pages/MainPage/MainPage';
 import MyList from './view/pages/MyListPage/MyList';
 import Store from './view/pages/StorePage/Store'
-import StoreList from './view/pages/StoreListPage/StoreList'
+// import StoreList from './view/pages/StoreListPage/StoreList'
 import SignIn from './view/pages/SignInPage/SignIn';
 import SignUp from './view/pages/SignUpPage/SignUp';
 
