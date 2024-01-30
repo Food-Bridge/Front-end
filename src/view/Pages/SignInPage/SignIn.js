3fa95e865fe1455dd1a462c2bf65d0d06811bc28
김선민 <sunmindev@gimseonmin-ui-MacBookAir.local>
2024-01-30 23:07:40 +0900

    Feat: Add SignIn/SignUp

--- src/view/Pages/SignInPage/SignIn.js
+++ src/view/Pages/SignInPage/SignIn.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignIn.scss';
 import LogInBtn from '../../components/LogInBtn/LogInBtn';
 import LogoBar from '../../components/LogoBar/LogoBar';
@@ -6,7 +6,31 @@ import SignUpBtn from '../../components/SignUpBtn/SignUpBtn';
 import KakaoBox from '../../components/KakaoLogin/KakaoLogin';
 import GoogleBtn from '../../components/GoogleBtn/GoogleBtn';
 
+import axios from 'axios';
+import { Navigate, useNavigate } from 'react-router-dom';
+
 function SignIn() {
+  const [emailVaule, setEmail] = useState('');
+  const [passwordVaule, setPassword] = useState('');
+
+  const navigate = useNavigate();
+  const [signedIn, setSignedIn] = useState(false);
+
+  const handleLogin = () => {
+    setSignedIn(true);
+
+    navigate('/signup');
+  };
+
+  const saveUserEmail = (event) => {
+    setEmail(event.target.value);
+    console.log(event.target.value);
+  }
+  const saveUserPassword = (event) => {
+    setPassword(event.target.value);
+    console.log(event.target.value);
+  }
+
   return (
     <>
       <LogoBar />
@@ -17,22 +41,38 @@ function SignIn() {
             <div className='signIn-margin'>
               <div className='signIn-formMargin'>
                 <div className='signIn-emailForm'>
-                  <input
-                    type='text'
-                    placeholder='아이디 또는 이메일'
+                  <input 
+                    type="text" 
+                    placeholder='아이디 또는 이메일' 
                     className='signIn-emailInput'
+                    value={emailVaule} 
+                    onChange={saveUserEmail}
                   />
                 </div>
                 <div className='signIn-passwdForm'>
-                  <input
-                    type='text'
-                    placeholder='비밀번호'
+                  <input 
+                    type="text" 
+                    placeholder='비밀번호' 
                     className='signIn-passwdInput'
+                    value={passwordVaule} 
+                    onChange={saveUserPassword}
                   />
                 </div>
               </div>
               <div className='signIn-btnMargin'>
-                <div className='signIn-signInBtn'>
+                <div className='signIn-signInBtn'
+                  onClick={() => {
+                    axios.post("http://localhost:8000/users/login/", {
+                        email : emailVaule,
+                        password : passwordVaule
+                    }).then(function (response) {
+                      console.log(response);
+                      handleLogin();
+                    }).catch(function (error) {
+                      console.log(error);
+                    })
+                  }}                
+                >
                   <LogInBtn />
                 </div>
                 <div className='signIn-signUpBtn'>
