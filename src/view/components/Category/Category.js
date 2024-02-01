ad88465a75fd11d1973e735406b52af90035114f
김선민 <sunmindev@gimseonmin-ui-MacBookAir.local>
2024-02-01 12:25:35 +0900

    Feat: modify Component

--- src/view/components/Category/Category.js
+++ src/view/components/Category/Category.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Category.scss'
 
-function Category({category}) {
+function Category({className, category, image}) {
   return (
-    <div className='Category'>
+    <div className={`${className}`}>
         <header className='category-frame'>
-            <div className='category-imgBlock'></div>
+            <div className='category-imgBlock'>
+              <img className='category-img' src={`${image}`} alt="img" />
+            </div>
             <h1 className='category-name'>{category}</h1>
         </header>
     </div>
