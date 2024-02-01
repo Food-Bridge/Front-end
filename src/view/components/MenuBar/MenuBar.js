4a0f502ea832263086767ca68857e4a3fdc3335c
김선민 <sunmindev@gimseonmin-ui-MacBookAir.local>
2024-02-01 12:25:35 +0900

    Feat: modify Component

--- src/view/components/MenuBar/MenuBar.js
+++ src/view/components/MenuBar/MenuBar.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import "./MenuBar.scss";
-
+import { useNavigate } from 'react-router-dom';
 
 function MenuBar({name}) {
-    // const name = props.name;
+    const navigate = useNavigate();
+
+    const handleHomeClick = () => {
+        navigate('/');
+      };
+    const handleStoreClick = () => {
+    navigate('/storeList');
+    };
+    const handleCommuClick = () => {
+    navigate('/commu');
+    };
 
     return (
         <div className='menuBar'>
             <header className='menuBar-frame'>
                 <div className='menuBar-margin'>
-                    <h1 className='menuBar-home'>홈</h1>
-                    <h1 className='menuBar-deliver'>배달/포장</h1>
-                    <h1 className='menuBar-commu'>커뮤니티</h1>                    
+                    <button onClick={handleHomeClick}><h1 className='menuBar-home' >홈</h1></button>
+                    <button onClick={handleStoreClick}><h1 className='menuBar-deliver'>배달/포장</h1></button>
+                    <button onClick={handleCommuClick}><h1 className='menuBar-commu'>커뮤니티</h1></button>
                 </div>
                 <div className='menuBar-position'>
                     <div className='menuBar-line'>
