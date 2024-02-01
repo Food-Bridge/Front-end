82abc7e5058394b70088e5990cb757ccd0f3a14d
seon318 <jys7861@gmail.com>
2024-02-01 15:03:03 +0900

    Design: Modify margin

--- src/view/Pages/MainPage/MainPage.js
+++ src/view/Pages/MainPage/MainPage.js
@@ -32,7 +32,8 @@ export default function MainPage() {
   return (
     <div className='main'>
       <SearchBar className='main-searchBar' location='강남구' count='1' />
-      <ImageSlider className='main-imageSlider' slides={SliderData} />
+      <div className='main-imageSliderContainer'><ImageSlider className='main-imageSlider' slides={SliderData} /></div>
+      
       <MenuBar name='menuBar-pageLine1'/>
       <div className='main-title'>
         <h1 className='main-text'>이달의 인기 맛집</h1>
