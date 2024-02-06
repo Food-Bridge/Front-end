6a50b64f1e9434a1a99e9eb624289eaac1b1fae6
seon318 <jys7861@gmail.com>
2024-02-06 16:50:36 +0900

    Feat: 로그인 여부 상태 추가

--- src/view/components/LogoBar/LogoBar.js
+++ src/view/components/LogoBar/LogoBar.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react';
 import { AiOutlineHome } from 'react-icons/ai';
 import { LuUserCircle2 } from 'react-icons/lu';
 import './LogoBar.scss';
-import { useNavigate } from 'react-router-dom';
+import  {useSelector} from 'react-redux'
+import { selectIsLoggedIn } from '../../../redux/reducers/authSlice';
+import { useNavigate } from
+ 'react-router-dom';
 
 export default function LogoBar() {
   const navigate = useNavigate();
+  const isLoggedIn = useSelector(selectIsLoggedIn)
 
   const handleHomeClick = () => {
     navigate('/');
   };
 
   const handleUserClick = () => {
-    navigate('/users/signin');
+    if (isLoggedIn) {
+      navigate('/user');
+    } else {
+    navigate('/users/signin');}
   };
+
   return (
     <div className='LogoBar'>
       <header className='logoBar-frame'>
