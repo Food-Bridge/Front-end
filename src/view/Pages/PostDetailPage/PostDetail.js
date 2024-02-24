f1c95d98a6a67cb092d23a0153b8896061e42d6e
김선민 <sunmindev@gimseonmin-ui-MacBookAir.local>
2024-02-24 14:20:28 +0900

    Feat: 게시물 댓글 페이지

--- src/view/Pages/PostDetailPage/PostDetail.js
+++ src/view/Pages/PostDetailPage/PostDetail.js
@@ -4,6 +4,8 @@ import SearchBar from '../../components/SearchBar/SearchBar'
 import MenuBar from '../../components/MenuBar/MenuBar'
 import DetailPost from '../../components/DetailPost/DetailPost'
 import PostComment from '../../components/PostComment/PostComment'
+import { postCommentData } from '../../../data/PostCommentData/PostCommentData'
+import { LuSend } from "react-icons/lu";
 
 function PostDetail() {
   return (
@@ -11,10 +13,20 @@ function PostDetail() {
       <SearchBar />
       <div className="postDetail-menuBar"><MenuBar name={"menuBar-pageLine3"} /></div>
       <DetailPost user={"김선민"} location={"강남역"}  />
-      {/* user, location, img, text */}
+
       <div className='postDetail-comment'>
-        <PostComment />
+        <div className='postDetail-commentList'>
+          {postCommentData.map((el) => {
+            return <PostComment userName={el.userName} comment={el.comment} />
+          })}          
+        </div>
+
+        <div className='postDetail-commentInput'>
+          <input type="text" className='postDetail-InputBox' placeholder='댓글을 입력해주세요...'/>
+          <LuSend className='postDetail-sendIcon'/>
+        </div>
       </div>
+
     </div>
   )    
 }
--- src/view/components/PostComment/PostComment.js
+++ src/view/components/PostComment/PostComment.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './PostComment.scss'
+import { IoIosHeartEmpty, IoIosHeart } from 'react-icons/io'
 
-const PostComment = () => {
+const PostComment = ({userName, comment}) => {
   const [newComment, setNewComment] = useState('');
+  const [isLike, setIsLike] = useState(false);
+  
+  const handleLike = () => {
+    setIsLike(!isLike);
+  };
 
   const handleCommentSubmit = () => {
     // 댓글 생성 API에 POST 요청 보내기
@@ -34,12 +40,21 @@ const PostComment = () => {
 
       <div className='postComment-frame'>
         <div className='postComment-top'>
-          <div className='postComment-profile'></div>
-          <div className='postComment-userName'>ooo 님</div>
+          <div className='postComment-userInfo'>
+            <div className='postComment-profile'></div>
+            <div className='postComment-userName'>{userName} 님</div>
+          </div>
+          <button className='postComment-like' onClick={handleLike}>
+            {isLike ? (
+              <IoIosHeart size='24' color='red' className='postComment-likeIcon' />
+            ) : (
+              <IoIosHeartEmpty size='24' color='red' className='postComment-likeIcon' />
+            )}
+          </button>
         </div>
         <div className='postComment-commentFrame'>
           <h1 className='postComment-comment'>
-          출판되게 폭넓는 개선이 사찰이어 심사가 점수의, 소아다. 제기하다. 45퍼센트 참여하다 쉽고 있은 있고 별다르면, 납부하며 돌파하다 출판되게 폭넓는 개선이 사찰이어 심사가 점수의, 소아다 제기하다. 45퍼센트 참여하다 쉽고 있은 있고 별다르면, 납부하며 돌파하다.
+            {comment}
           </h1>
         </div>
       </div>
