8bb0081b2f7eed1de320f631171d0b3abaffe000
seon318 <jys7861@gmail.com>
2024-02-28 00:50:29 +0900

    Feat: 주소 삭제 및 닉네임 변경 기능 추가

--- src/view/Pages/LocationSearchPage/LocationSearch.js
+++ src/view/Pages/LocationSearchPage/LocationSearch.js
@@ -10,16 +10,26 @@ const LocationSearch = () => {
     const address = data.address;
     const roadAddress = data.roadAddress;
     const jibunAddress = data.jibunAddress;
+    const sigungu = data.sigungu;
     const buildingName = data.buildingName || null;
 
     axios
-      .post('http://127.0.0.1:8000/users/address/', {
-        detail_address: address,
-        road_address: roadAddress,
-        jibun_address: jibunAddress,
-        building_name: buildingName,
-        user: 2,
-      })
+      .post(
+        'http://127.0.0.1:8000/users/address/',
+        {
+          detail_address: address,
+          road_address: roadAddress,
+          jibun_address: jibunAddress,
+          building_name: buildingName,
+          sigungu: sigungu,
+          user: 4,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('access')}`,
+          },
+        }
+      )
       .then(function (response) {
         console.log(response);
       })
