ac90abae4d537a89f2c0f4682aa9fc5640bbc39e
seon318 <jys7861@gmail.com>
2024-03-02 10:10:03 +0900

    Feat: 주소 상태관리 추가 및 업데이트 오류 해결

--- src/view/Pages/LocationSearchPage/LocationSearch.js
+++ src/view/Pages/LocationSearchPage/LocationSearch.js
@@ -1,31 +1,25 @@
 import React from 'react';
 import DaumPostCode from 'react-daum-postcode';
 import axiosInstance from '../../../api/instance';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 
-const LocationSearch = () => {
-  const navigate = useNavigate();
-
-  const handleComplete = (data) => {
-    const address = data.address;
-    const roadAddress = data.roadAddress;
-    const jibunAddress = data.jibunAddress;
-    const sigungu = data.sigungu;
-    const buildingName = data.buildingName || null;
-
-    axiosInstance.post('/users/address/', {
+const LocationSearch = ({ complete }) => {
+  const handleComplete = async (data) => {
+    const { address, roadAddress, jibunAddress, sigungu, buildingName } = data;
+    await axiosInstance.post('/users/address/', {
       detail_address: address,
       road_address: roadAddress,
       jibun_address: jibunAddress,
-      building_name: buildingName,
+      building_name: buildingName || null,
       sigungu: sigungu,
     });
-
-    navigate('/address');
+    complete();
   };
 
-  return <DaumPostCode onComplete={handleComplete} className='post-code' />;
+  return (
+    <div>
+      <DaumPostCode onComplete={handleComplete} className='post-code' />;
+    </div>
+  );
 };
 
 export default LocationSearch;
