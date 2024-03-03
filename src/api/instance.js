cd4c7e793b19e4e23a3361bdb890aab49736d1e2
seon318 <jys7861@gmail.com>
2024-03-03 10:05:53 +0900

    Feat: axios 응답 인터셉터 설정 및 라우터 주소변경

--- src/api/instance.js
+++ src/api/instance.js
@@ -10,22 +10,19 @@ axiosInstance.interceptors.request.use(
   async (config) => {
     if (!config.headers) return config;
 
-    // 토큰 만료 여부 확인
     const isTokenExpired = (token) => {
-      if (!token) return true; // 토큰이 없으면 만료된 것으로 간주합니다.
+      if (!token) return true;
 
-      const currentTime = Math.floor(Date.now() / 1000); // 현재 시간(UNIX 타임스탬프)
-      const tokenExpiration = token.exp; // 토큰의 만료 시간
+      const currentTime = Math.floor(Date.now() / 1000);
+      const tokenExpiration = token.exp;
 
-      return currentTime >= tokenExpiration; // 현재 시간이 토큰의 만료 시간 이후인지 확인합니다.
+      return currentTime >= tokenExpiration;
     };
 
     let token = localStorage.getItem('access');
     if (token && isTokenExpired(token)) {
       try {
-        const {
-          data: { access },
-        } = await axios.get(REFRESH_URL);
+        const { data: { access } } = await axios.get(REFRESH_URL);
         localStorage.setItem('access', access);
         token = access;
       } catch (error) {
@@ -48,50 +45,19 @@ axiosInstance.interceptors.request.use(
   }
 );
 
-// 응답 인터셉터를 axiosInstance에 설정합니다.
 axiosInstance.interceptors.response.use(
   (response) => {
     return response;
   },
   async (error) => {
-    const {
-      config,
-      response: { status },
-    } = error;
+    const { config, response: { status } } = error;
 
     if (config.url === REFRESH_URL || status !== 401 || config.sent) {
       return Promise.reject(error);
     }
 
-    config.sent = true;
-    const access = await getRefreshToken();
-
-    if (access) {
-      config.headers.Authorization = access;
-    }
-
-    return axios(config);
+    return Promise.reject(error);
   }
 );
-
-// 토큰을 갱신하는 함수
-const getRefreshToken = async () => {
-  try {
-    const {
-      data: { access, refresh },
-    } = await axios.get(REFRESH_URL);
-
-    localStorage.setItem('access', access);
-
-    if (refresh !== null) {
-      localStorage.setItem('refresh', refresh);
-    }
-
-    return access;
-  } catch (e) {
-    localStorage.removeItem('access');
-    localStorage.removeItem('refresh');
-  }
-};
 
 export default axiosInstance;
