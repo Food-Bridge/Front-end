bad2edc3dd232f035c3517380aef59afee7ca709
seon318 <jys7861@gmail.com>
2024-03-25 23:43:36 +0900

    Feat: 매장 등록 이미지 오류 수정 및 get, patch, delete 요청 추가

--- src/view/Pages/StoreUploadPage/StoreUpload.js
+++ src/view/Pages/StoreUploadPage/StoreUpload.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './StoreUpload.scss';
 import { IoMdArrowDropdown, IoIosSearch } from 'react-icons/io';
 import { HiMiniXMark } from 'react-icons/hi2';
 import axiosInstance from '../../../api/instance';
 
 import DaumPostCode from 'react-daum-postcode';
+import { Form } from 'react-router-dom';
 
 function StoreUpload() {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,16 +19,28 @@ function StoreUpload() {
   const [description, setDescription] = useState('');
   const [minOrderPrice, setMinOrderPrice] = useState(0);
   const [minPickUpPrice, setMinPickUpPrice] = useState(0);
+  const [minDeliverTime, setMinDeliverTime] = useState(0);
+  const [maxDeliverTime, setMaxDeliverTime] = useState(0);
   const [minPickUpTime, setMinPickUpTime] = useState(0);
   const [start, setStart] = useState(0);
   const [end, setEnd] = useState(0);
   const [operating, setOperating] = useState('');
   const [deliveryFee, setDeliveryFee] = useState(0);
-  const [mainCategory, setMainCategory] = useState('카테고리 선택');
+  const [mainCategory, setMainCategory] = useState({
+    id: 0,
+    name: '카테고리 선택',
+  });
   const [image, setImage] = useState(null);
   const [imageDisplay, setImageDisplay] = useState(null);
 
-  const categoryList = ['한식', '중식', '일식', '양식', '분식', '디저트'];
+  const categoryList = [
+    { id: 1, name: '한식' },
+    { id: 2, name: '중식' },
+    { id: 3, name: '일식' },
+    { id: 4, name: '양식' },
+    { id: 5, name: '분식' },
+    { id: 6, name: '디저트' },
+  ];
 
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
@@ -47,6 +60,7 @@ function StoreUpload() {
   const handleSetImage = (event) => {
     const file = event.target.files[0];
     setImage(file);
+
     const reader = new FileReader();
     reader.onloadend = () => {
       setImageDisplay(reader.result);
@@ -56,13 +70,24 @@ function StoreUpload() {
 
   const handleAddStore = async () => {
     try {
-      const res = await axiosInstance.post('/restaurant/', {
+      const formData = new FormData();
+      formData.append('image', image);
+
+      await axiosInstance.patch('/restaurant/3', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      await axiosInstance.patch('/restaurant/3', {
         name: name,
         address: address,
         phone_number: phone,
         description: description,
         minimumOrderPrice: minOrderPrice,
         minimumPickupPrice: minPickUpPrice,
+        minDeliveryTimeMinutes: minDeliverTime,
+        maxDeliveryTimeMinutes: maxDeliverTime,
         minPickupTime: minPickUpTime,
         packaging: packaging,
         status: status,
@@ -70,13 +95,44 @@ function StoreUpload() {
         end: end,
         operatingTime: operating,
         deliveryFee: deliveryFee,
-        mainCategory: mainCategory,
+        mainCategory: mainCategory.id,
+        mainCategory_name: mainCategory.name,
       });
-      console.log(res);
     } catch (error) {
-      console.error('Error adding store:', error);
+      alert('에러가 발생했습니다', error);
     }
   };
+
+  const handleDeleteStore = () => {
+    axiosInstance.delete('/restaurant/3');
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axiosInstance.get('/restaurant/3');
+      setName(res.data.name);
+      setAddress(res.data.address);
+      setPhone(res.data.phone_number);
+      setDescription(res.data.description);
+      setMinOrderPrice(res.data.minimumOrderPrice);
+      setMinPickUpPrice(res.data.minimumPickupPrice);
+      setMinDeliverTime(res.data.minDeliveryTimeMinutes);
+      setMaxDeliverTime(res.data.maxDeliveryTimeMinutes);
+      setMinPickUpTime(res.data.minPickUpTime);
+      setPackaging(res.data.packaging);
+      setStatus(res.data.status);
+      setStart(res.data.start);
+      setEnd(res.data.end);
+      setOperating(res.data.operatingTime);
+      setDeliveryFee(res.data.deliveryFee);
+      setMainCategory({
+        id: res.data.mainCategory,
+        name: res.data.mainCategory_name,
+      });
+      setImageDisplay(res.data.image);
+    };
+    fetchData();
+  }, []);
 
   return (
     <div className='StoreUpload'>
@@ -127,7 +183,7 @@ function StoreUpload() {
             onClick={toggleDropdown}
           >
             <button className='storeUpload-mainCategory'>
-              {mainCategory}
+              {mainCategory.name}
               <span className='storeUpload-dropdownIcon'>
                 <IoMdArrowDropdown />
               </span>
@@ -136,8 +192,8 @@ function StoreUpload() {
               <ul className='storeUpload-dropdownMenu'>
                 {categoryList.map((cat) => {
                   return (
-                    <button onClick={() => handleItemClick(cat)}>
-                      <li>{cat}</li>
+                    <button key={cat.id} onClick={() => handleItemClick(cat)}>
+                      <li>{cat.name}</li>
                     </button>
                   );
                 })}
@@ -162,6 +218,7 @@ function StoreUpload() {
             <input
               className='storeUpload-storeName'
               type='text'
+              placeholder={name}
               onChange={(e) => setName(e.target.value)}
             />
           </div>
@@ -171,8 +228,8 @@ function StoreUpload() {
             <h1 className='storeUpload-title'>매장 전화번호</h1>
             <input
               className='storeUpload-phoneNumber'
-              type='text'
-              placeholder='숫자만 입력해주세요'
+              type='number'
+              placeholder={phone ? phone : '숫자만 입력해주세요'}
               onChange={(e) => setPhone(e.target.value)}
             />
           </div>
@@ -182,16 +239,19 @@ function StoreUpload() {
             <textarea
               className='storeUpload-storeDescription'
               type='text'
+              placeholder={description}
               onChange={(e) => setDescription(e.target.value)}
             />
           </div>
-          {/* 매장 최소 주문 금액 */}
+          {/* 배달 최소 주문 금액 */}
           <div className='storeUpload-deliverPrice'>
-            <h1 className='storeUpload-title'>최소 주문 금액</h1>
+            <h1 className='storeUpload-title'>배달 최소 주문 금액</h1>
             <input
               className='storeUpload-deliverMinPrice'
-              type='text'
-              placeholder='숫자만 입력해주세요'
+              type='number'
+              placeholder={
+                minOrderPrice ? minOrderPrice : '숫자만 입력해주세요'
+              }
               onChange={(e) => setMinOrderPrice(e.target.value)}
             />
           </div>
@@ -200,19 +260,44 @@ function StoreUpload() {
             <h1 className='storeUpload-title'>픽업 최소 주문 금액</h1>
             <input
               className='storeUpload-pickupMinPrice'
-              type='text'
-              placeholder='숫자만 입력해주세요'
+              type='number'
+              placeholder={
+                minPickUpPrice ? minPickUpPrice : '숫자만 입력해주세요'
+              }
               onChange={(e) => setMinPickUpPrice(e.target.value)}
             />
           </div>
-          {/* 매장 최소 픽업 시간 */}
+          {/* 배달 시간 */}
+          <div className='storeUpload-deliverTime'>
+            <h1 className='storeUpload-title'>배달 최소 시간</h1>
+            <input
+              className='storeUpload-deliverTime'
+              type='number'
+              placeholder={
+                minDeliverTime ? minDeliverTime : '숫자만 입력해주세요'
+              }
+              onChange={(e) => setMinDeliverTime(e.target.value)}
+            />
+            <h1 className='storeUpload-title'>배달 최대 시간</h1>
+            <input
+              className='storeUpload-deliverTime'
+              type='number'
+              placeholder={
+                maxDeliverTime ? maxDeliverTime : '숫자만 입력해주세요'
+              }
+              onChange={(e) => setMaxDeliverTime(e.target.value)}
+            />
+          </div>
+          {/* 최소 픽업 시간 */}
           <div className='storeUpload-pickupTime'>
-            <h1 className='storeUpload-title'>매장 픽업 최소 시간</h1>
+            <h1 className='storeUpload-title'>픽업 최소 시간</h1>
 
             <input
               className='storeUpload-pickupMinTime'
-              type='text'
-              placeholder='숫자만 입력해주세요'
+              type='number'
+              placeholder={
+                minPickUpTime ? minPickUpTime : '숫자만 입력해주세요'
+              }
               onChange={(e) => setMinPickUpTime(e.target.value)}
             />
           </div>
@@ -221,8 +306,8 @@ function StoreUpload() {
             <h1 className='storeUpload-title'>영업 시작 시간</h1>
             <input
               className='storeUpload-startTime'
-              type='text'
-              placeholder='0~24 사이의 숫자만 입력해주세요'
+              type='number'
+              placeholder={start ? start : '0~24 사이의 숫자만 입력해주세요'}
               onChange={(e) => setStart(e.target.value)}
             />
           </div>
@@ -231,8 +316,8 @@ function StoreUpload() {
             <h1 className='storeUpload-title'>영업 마감 시간</h1>
             <input
               className='storeUpload-endTime'
-              type='text'
-              placeholder='0~24 사이의 숫자만 입력해주세요'
+              type='number'
+              placeholder={end ? end : '0~24 사이의 숫자만 입력해주세요'}
               onChange={(e) => setEnd(e.target.value)}
             />
           </div>
@@ -243,6 +328,7 @@ function StoreUpload() {
             <textarea
               className='storeUpload-timeInformation'
               type='text'
+              placeholder={operating}
               onChange={(e) => setOperating(e.target.value)}
             />
           </div>
@@ -251,8 +337,8 @@ function StoreUpload() {
             <h1 className='storeUpload-title'>배달 요금</h1>
             <input
               className='storeUpload-operatingTime'
-              type='text'
-              placeholder='숫자만 입력해주세요'
+              type='number'
+              placeholder={deliveryFee ? deliveryFee : '숫자만 입력해주세요'}
               onChange={(e) => setDeliveryFee(e.target.value)}
             />
           </div>
@@ -273,7 +359,15 @@ function StoreUpload() {
             className='storeUpload-storeUploadBtn'
             onClick={handleAddStore}
           >
-            매장 등록하기
+            저장하기
+          </button>
+        </div>
+        <div className='storeUpload-deleteBtn'>
+          <button
+            className='storeUpload-storeDeleteBtn'
+            onClick={handleDeleteStore}
+          >
+            매장 삭제하기
           </button>
         </div>
       </div>
