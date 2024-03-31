56dd356d69301e0e3c4a3e18bc797ab84e1fa3f4
seon318 <jys7861@gmail.com>
2024-03-31 22:50:58 +0900

    Feat: 결제 서버 연결

--- src/view/Pages/PaymentPage/Payment.js
+++ src/view/Pages/PaymentPage/Payment.js
@@ -4,7 +4,7 @@ import './Payment.scss';
 import { SlPresent } from 'react-icons/sl';
 import { MdOutlineHouse } from 'react-icons/md';
 import { CiDeliveryTruck } from 'react-icons/ci';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import {
   selectMenu,
   selectStore,
@@ -16,15 +16,17 @@ import {
   selectDefaultId,
 } from '../../../redux/reducers/addressSlice';
 import axiosInstance from '../../../api/instance';
+import { RiArrowDropDownFill } from 'react-icons/ri';
 
 export default function Payment() {
   const navigate = useNavigate();
-  const [isPhoneEdit, setIsPhoneEdit] = useState(false);
-  const [phoneNumber, setPhoneNumber] = useState('01012341234');
   const [storeRequest, setStoreRequest] = useState('');
   const [disposable, setDisposable] = useState(false);
   const [deliverRequest, setDeliverRequest] = useState('');
   const [paymentMethod, setPaymentMethod] = useState('credit_card');
+  const [couponData, setCouponData] = useState([]);
+  const [couponList, showCouponList] = useState(false);
+  const [selectedCoupon, setSelectedCoupon] = useState('');
 
   const isDeliver = useSelector(selectIsDeliver);
   const store = useSelector(selectStore);
@@ -35,6 +37,7 @@ export default function Payment() {
 
   const menuList = [];
   const optionList = [];
+  const sOptionList = [];
 
   menuData.forEach((menu) => {
     const menuInfo = {
@@ -49,33 +52,64 @@ export default function Payment() {
       option_id: option.id,
       option_name: option.name,
       price: option.price,
-      quantity: option.quantity,
+      quantity: 1,
     }));
     optionList.push(optionInfo);
+
+    const sOptionInfo =
+      menu.sOption.length > 0
+        ? menu.sOption.map((option) => ({
+            option_id: option.id,
+            option_name: option.name,
+            price: option.price,
+            quantity: 1,
+          }))
+        : undefined;
+    sOptionList.push(sOptionInfo);
   });
-  console.log(menuData);
-  console.log(optionList);
+
   const totalValue = menuData.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
   const deliveryFee = store.deliveryFee ? store.deliveryFee : 0;
   const totalPrice = totalValue + deliveryFee;
-  const handlePostPayment = () => {
-    axiosInstance.post('/order/', {
-      required_options_count: store.required_options_count,
-      menu_list: menuData,
-      option_list: menuData,
+
+  const handleClickCoupon = (el) => {
+    setSelectedCoupon(el);
+    showCouponList(false);
+  };
+  
+  useEffect(() => {
+    const fetchCoupon = async () => {
+      const res = await axiosInstance.get('/userscoupon/');
+      setCouponData(res.data);
+    };
+    fetchCoupon();
+  }, []);
+
+  const handlePostPayment = async () => {
+    const data = {
+      required_options_count: 1,
+      menu_list: menuList,
+      option_list: optionList,
+      soption_list: sOptionList,
       total_price: totalPrice,
-      coupon_code: '',
+      coupon_code: selectedCoupon.coupon_code,
       deliver_address: defaultAddress.detail_address,
       deliveryman_request: deliverRequest,
+      restaurant_request: storeRequest,
+      disposable_request: disposable,
       is_deliver: isDeliver,
       payment_method: paymentMethod,
       restaurant: store.id,
       order_State: 'order_complete',
-    });
+    };
+
+    console.log(data);
+    await axiosInstance.post('/order/', data);
   };
+
   return (
     <>
       <h1 className='payment-header'>결제하기</h1>
@@ -98,47 +132,29 @@ export default function Payment() {
             </h2>
           </div>
         </div>
-        <div className='payment-userInfoGroup'>
-          <div className='payment-addressGroup'>
-            <div className='payment-addressGroup-header'>
-              <h3 className='payment-addressNickname'>
-                <MdOutlineHouse size='20' />{' '}
-                {defaultAddress.nickname
-                  ? defaultAddress.nickname
-                  : defaultAddress.detail_address}
-              </h3>
-              <button
-                className='payment-addressEdit'
-                onClick={() => navigate('/users/address/')}
-              >
-                변경
-              </button>
-            </div>
-            <h3 className='payment-addressName'>
-              {defaultAddress.detail_address}
-            </h3>
-          </div>
-          <div className='payment-phoneGroup'>
-            {isPhoneEdit ? (
-              <input
-                className='payment-phoneEditInput'
-                type='number'
-                placeholder='숫자만 입력해주세요.'
-                onChange={(e) => setPhoneNumber(e.target.value)}
-              />
-            ) : (
-              <div className='payment-phone'>
-                {phoneNumber.replace(/(\d{3})(\d{3,4})(\d{4})/, '$1-$2-$3')}
+        {isDeliver && (
+          <div className='payment-userInfoGroup'>
+            <div className='payment-addressGroup'>
+              <div className='payment-addressGroup-header'>
+                <h3 className='payment-addressNickname'>
+                  <MdOutlineHouse size='20' />
+                  {defaultAddress.nickname
+                    ? defaultAddress.nickname
+                    : defaultAddress.detail_address}
+                </h3>
+                <button
+                  className='payment-addressEdit'
+                  onClick={() => navigate('/users/address/')}
+                >
+                  변경
+                </button>
               </div>
-            )}
-            <button
-              className='payment-phoneEdit'
-              onClick={() => setIsPhoneEdit(!isPhoneEdit)}
-            >
-              {isPhoneEdit ? '수정' : '변경'}
-            </button>
+              <h3 className='payment-addressName'>
+                {defaultAddress.detail_address}
+              </h3>
+            </div>
           </div>
-        </div>
+        )}
         <div className='payment-requestGroup'>
           <div className='payment-storeRequest'>
             <h3 className='payment-storeRequest-title'>가게 요청사항</h3>
@@ -165,9 +181,36 @@ export default function Payment() {
             />
           </div>
         </div>
+
         <div className='payment-method'>
           <PaymentMethod />
+          <div className='payment-coupon'>
+            <h3 className='payment-couponTitle'>쿠폰 사용</h3>
+            <button
+              className='payment-couponCodeBox'
+              onClick={() => showCouponList(!couponList)}
+            >
+              <p className='payment-couponCode'>
+                {selectedCoupon.coupon_code
+                  ? selectedCoupon.coupon_code
+                  : '쿠폰을 선택해주세요.'}
+              </p>
+              <p className='payment-couponBtn'>
+                <RiArrowDropDownFill size='20' />
+              </p>
+            </button>
+            {couponList && (
+              <div className='payment-couponList'>
+                {couponData.map((el) => (
+                  <button key={el.id} onClick={() => handleClickCoupon(el)}>
+                    {el.coupon_code}
+                  </button>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
+
         {isDeliver && (
           <div className='payment-priceGroup'>
             <div className='payment-price'>
@@ -183,6 +226,12 @@ export default function Payment() {
                 {deliveryFee.toLocaleString('ko-KR')}원
               </p>
             </div>
+            <div className='payment-price'>
+              <h2 className='payment-title'>쿠폰 할인</h2>
+              <p className='payment-value'>
+                {deliveryFee.toLocaleString('ko-KR')}원
+              </p>
+            </div>
           </div>
         )}
         <div className='payment-priceGroup'>
@@ -193,7 +242,7 @@ export default function Payment() {
             </p>
           </div>
         </div>
-        <button className='payment-orderBtn'>
+        <button onClick={handlePostPayment} className='payment-orderBtn'>
           {totalPrice.toLocaleString('ko-KR')}원 {isDeliver ? '배달' : '포장'}
           결제하기
         </button>
