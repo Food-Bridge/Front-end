c1eac4135da3e41f758f6e3d3cf1f6ce28430f8e
sunminDev <dev.sunmin@gmail.com>
2024-04-02 14:30:10 +0900

    Feat: 커뮤니티 isLike 수정

--- src/view/components/DetailPost/DetailPost.js
+++ src/view/components/DetailPost/DetailPost.js
@@ -1,94 +1,51 @@
-import React, { useEffect, useState } from 'react'
 import './DetailPost.scss'
-import { CiLocationOn } from 'react-icons/ci'
-import { postTagData } from '../../../data/PostCardData/PostTagData'
-import { IoIosHeart, IoIosHeartEmpty } from 'react-icons/io'
 import axios from 'axios'
+import React, { useEffect } from 'react'
+import { CiLocationOn } from 'react-icons/ci'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-
+import { IoIosHeart, IoIosHeartEmpty } from 'react-icons/io'
 import { toggleLike } from '../../../redux/reducers/communitySlice'
+import { postTagData } from '../../../data/PostCardData/PostTagData'
+import axiosInstance from '../../../api/instance'
 
 function DetailPost({user, location, image, title, content}) {
+  const isLiked = useSelector((state) => state.community.like.isLiked);
+  const dispatch = useDispatch();
 
-    // const data = [
-    //     {
-    //         img : "https://c.pxhere.com/photos/83/09/basil_blur_cuisine_delicious_dish_food_food_photography_grow-1545177.jpg!d",
-    //         title: "강남역 근처 일식집이 오픈 했어요",
-    //         text : "출판되게 폭넓는 개선이 사찰이어 심사가 점수의, 소아다. 제기하다. 45퍼센트 참여하다 쉽고 있은 있고 별다르면, 납부하며 돌파하다 출판되게 폭넓는 개선이 사찰이어 심사가 점수의, 소아다 제기하다. 45퍼센트 참여하다 쉽고 있은 있고 별다르면, 납부하며 돌파하다. 출판되게 폭넓는 개선이 사찰이어 심사가 점수의, 소아다. 45퍼센트 참여하다 쉽고 있은 있고 별다르면, 납부하며 돌파하다 출판되게 폭넓는 개선이 사찰이어 심사가 점수의, 소아다 제기하다"
-    //     },
-    //     {
-    //         tag1 : "오픈소식",
-    //         tag2 : "일식맛집",
-    //         tag3 : "일식덮밥",
-    //         tag4 : "동네맛집"
-    //     }
-    // ] 
-    
-    // const [isLike, setIsLike] = useState(false);
-    const isLiked = useSelector((state) => state.community.like.isLiked);
-    const dispatch = useDispatch();
-  
-    // const handleLike = () => {
-    //   setIsLike(!isLike);
-    // };
+  const formData = new FormData();
+  const navigate = useNavigate();
+  const id = window.location.href.split('/').reverse()[0]
 
-    const formData = new FormData();
-    const navigate = useNavigate();
-    const id = window.location.href.split('/').reverse()[0]
-
-  // 페이지 로드될 때 로컬 스토리지에서 좋아요 상태 가져오기
   useEffect(() => {
     const storedLikeStatus = localStorage.getItem('post_like_status');
-    if (storedLikeStatus) {
+    if (storedLikeStatus !== null) {
       dispatch(toggleLike(JSON.parse(storedLikeStatus)));
     }
   }, [dispatch]);
 
-  // // 좋아요 상태 변경 시 로컬 스토리지에 저장하기
-  // useEffect(() => {
-  //   localStorage.setItem('post_like_status', JSON.stringify(isLike));
-  // }, [isLike]);
-
   const handleLike = () => {
-    axios
-      .post(
-        `http://localhost:8000/community/${id}/likes/`,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('access')}`,
-          },
-        }
-      )
-      .then(function (response) {
-        dispatch(toggleLike(!isLiked)); // 좋아요 상태 토글
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error.response.data);
-      });
+    axiosInstance.post(`/community/${id}/likes/`)
+    .then(function (response) {
+      dispatch(toggleLike(!isLiked));
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error.response.data);
+    });
   };
 
-
-    // 게시물 삭제
-    const handleDeletePost = () => {
-      axios
-        .delete(`http://localhost:8000/community/${id}/`, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('access')}`,
-          },
-        })
-        .then(response => {
-          console.log('Post deleted successfully:', response);
-          // 게시물 삭제 후 필요한 작업 수행 (예: 리다이렉트 등)
-          navigate("/commuPostWeek")
-        })
-        .catch(error => {
-          console.error('Error deleting post:', error);
-        });
-    };
-  
+  // 게시물 삭제
+  const handleDeletePost = () => {
+    axiosInstance.delete(`/community/${id}/`)
+    .then(response => {
+      console.log('Post deleted successfully:', response);
+      navigate("/commuPostWeek");
+    })
+    .catch(error => {
+      console.error('Error deleting post:', error);
+    });
+  };
 
   return (
     <div className='DetailPost'>
