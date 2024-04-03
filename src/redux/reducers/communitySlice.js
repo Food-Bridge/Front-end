52a2882f86566d396be2972966b6ff72d2d68731
sunminDev <dev.sunmin@gmail.com>
2024-04-04 01:45:12 +0900

    commuPost 새로고침 에러 해결

--- src/redux/reducers/communitySlice.js
+++ src/redux/reducers/communitySlice.js
@@ -1,13 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit';
 import axiosInstance from '../../api/instance';
 
+const initialState = localStorage.getItem('communityState')
+  ? JSON.parse(localStorage.getItem('communityState'))
+  : {
+      weekly: [],
+      daily: [],
+      latest: [],
+    };
+
 const CommunitySlice = createSlice({
   name: 'community',
-  initialState: {
-    weekly: [],
-    daily: [],
-    latest: [],
-  },
+  initialState,
   reducers: {
     setWeeklyPost(state, action) {
       state.weekly = action.payload;
@@ -22,13 +26,13 @@ const CommunitySlice = createSlice({
 });
 
 export const selectWeeklyPost = (state) => state.community.weekly;
-export const selectDailyPost = (state) => state.community.weekly;
-export const selectLatestPost = (state) => state.community.weekly;
+export const selectDailyPost = (state) => state.community.daily;
+export const selectLatestPost = (state) => state.community.latest;
 
 export const { setWeeklyPost, setDailyPost, setLatestPost } =
   CommunitySlice.actions;
 
-export const fetchPostData = () => async (dispatch) => {
+export const fetchPostData = () => async (dispatch, getState) => {
   await axiosInstance.get('/community/weekly/').then((response) => {
     dispatch(setWeeklyPost(response.data));
   });
@@ -38,6 +42,8 @@ export const fetchPostData = () => async (dispatch) => {
   await axiosInstance.get('/community/latest/').then((response) => {
     dispatch(setLatestPost(response.data));
   });
+
+  localStorage.setItem('communityState', JSON.stringify(getState().community));
 };
 
 export default CommunitySlice.reducer;
