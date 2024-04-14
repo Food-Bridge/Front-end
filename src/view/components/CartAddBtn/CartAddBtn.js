76ed05632ffbb9390cc4f2e2af37b1750ff4efcc
seon318 <jys7861@gmail.com>
2024-04-14 09:09:22 +0900

    Fix: 로그인 여부에 따라 장바구니 접근 가능

--- src/view/components/CartAddBtn/CartAddBtn.js
+++ src/view/components/CartAddBtn/CartAddBtn.js
@@ -11,11 +11,13 @@ import {
 } from '../../../redux/reducers/cartSlice';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import { selectIsLoggedIn } from '../../../redux/reducers/authSlice';
 
 const CartAddBtn = ({ price, menuData, data }) => {
   const navigate = useNavigate();
   const menu = useSelector(selectMenu);
   const isMenuIn = useSelector(selectIsMenuIn);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
   const dispatch = useDispatch();
   const [quantity, setQuantity] = useState(1);
 
@@ -29,46 +31,57 @@ const CartAddBtn = ({ price, menuData, data }) => {
     }
   };
   const handleAddCart = () => {
-    if (isMenuIn && menu[0]?.restaurant === menuData.restaurant) {
-      const existingMenuItem = menu.find(
-        (item) =>
-          item.id === menuData.id &&
-          arraysEqual(item.option, menuData.option) &&
-          arraysEqual(item.sOption, menuData.sOption)
-      );
-      if (existingMenuItem !== undefined) {
-        const updatedMenu = menu.map((item) =>
-          item.id === existingMenuItem.id &&
-          item.option === existingMenuItem.option &&
-          item.sOption === existingMenuItem.sOption
-            ? { ...item, quantity: item.quantity + quantity }
-            : item
-        );
-        dispatch(setMenuData(updatedMenu));
-      } else {
-        dispatch(setMenuData([...menu, { ...menuData, quantity }]));
-      }
-      dispatch(addMenu());
-      navigate(-1);
+    if (!isLoggedIn) {
+      Swal.fire({
+        icon: 'warning',
+        text: '알림',
+        text: '로그인이 필요합니다.',
+        showCancelButton: false,
+        confirmButtonText: '로그인하기',
+        confirmButtonColor: 'black',
+      }).then((res) => res.isConfirmed && navigate('/users/signin'));
     } else {
-      dispatch(setCurrentStore(data));
-      if (isMenuIn) {
-        Swal.fire({
-          icon: 'warning',
-          title: '알림',
-          html: '장바구니에는 같은 가게의 메뉴만 담을 수 있습니다.<br>기존에 담은 메뉴를 삭제하시겠습니까?',
-          showCancelButton: true,
-          confirmButtonText: '삭제',
-          cancelButtonText: '취소',
-          confirmButtonColor: '#ca0000',
-          cancelButtonColor: 'black',
-        }).then((res) => {
-          res.isConfirmed && dispatch(deleteMenu());
-        });
-      } else {
-        dispatch(setMenuData([{ ...menuData, quantity }]));
+      if (isMenuIn && menu[0]?.restaurant === menuData.restaurant) {
+        const existingMenuItem = menu.find(
+          (item) =>
+            item.id === menuData.id &&
+            arraysEqual(item.option, menuData.option) &&
+            arraysEqual(item.sOption, menuData.sOption)
+        );
+        if (existingMenuItem !== undefined) {
+          const updatedMenu = menu.map((item) =>
+            item.id === existingMenuItem.id &&
+            item.option === existingMenuItem.option &&
+            item.sOption === existingMenuItem.sOption
+              ? { ...item, quantity: item.quantity + quantity }
+              : item
+          );
+          dispatch(setMenuData(updatedMenu));
+        } else {
+          dispatch(setMenuData([...menu, { ...menuData, quantity }]));
+        }
         dispatch(addMenu());
         navigate(-1);
+      } else {
+        dispatch(setCurrentStore(data));
+        if (isMenuIn) {
+          Swal.fire({
+            icon: 'warning',
+            title: '알림',
+            html: '장바구니에는 같은 가게의 메뉴만 담을 수 있습니다.<br>기존에 담은 메뉴를 삭제하시겠습니까?',
+            showCancelButton: true,
+            confirmButtonText: '삭제',
+            cancelButtonText: '취소',
+            confirmButtonColor: '#ca0000',
+            cancelButtonColor: 'black',
+          }).then((res) => {
+            res.isConfirmed && dispatch(deleteMenu());
+          });
+        } else {
+          dispatch(setMenuData([{ ...menuData, quantity }]));
+          dispatch(addMenu());
+          navigate(-1);
+        }
       }
     }
   };
