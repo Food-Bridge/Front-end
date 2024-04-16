956c874fba583822942cce4b609b9325380f1d25
seon318 <jys7861@gmail.com>
2024-04-16 16:25:19 +0900

    Fix: 카테고리, 이미지슬라이더 이미지 최적화

--- src/data/StoreListSliderImg/SliderImgData.js
+++ src/data/StoreListSliderImg/SliderImgData.js
@@ -1,7 +1,7 @@
-export const SliderImgData = [
-  'https://cdn.pixabay.com/photo/2020/02/28/12/43/bibimbap-4887417_1280.jpg',
-  'https://cdn.pixabay.com/photo/2020/01/07/05/52/spaghetti-4746831_1280.jpg',
-  'https://cdn.pixabay.com/photo/2023/09/04/20/39/cake-8233676_1280.jpg',
-  '  https://cdn.pixabay.com/photo/2019/08/15/09/03/pizza-4407510_1280.jpg',
-  'https://cdn.pixabay.com/photo/2019/10/03/10/07/food-4522872_1280.jpg',
-];
+import bibimbap from './bibimbap.webp';
+import cake from './cake.webp';
+import katsu from './katsu.webp';
+import pizza from './pizza.webp';
+import spaghetti from './spaghetti.webp';
+
+export const SliderImgData = [bibimbap, cake, katsu, pizza, spaghetti];
