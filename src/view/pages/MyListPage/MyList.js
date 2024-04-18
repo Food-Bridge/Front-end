da3c80b1815e775a129901ba1e78f79696d6f6b6
NewJiSoo <135521917+NewJiSoo@users.noreply.github.com>
2024-04-19 03:10:35 +0900

    Fix: 미사용 코드 정리

--- src/view/pages/MyListPage/MyList.js
+++ src/view/pages/MyListPage/MyList.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import {
-  selectProfile,
-  setProfile,
   deleteTokens,
   logout,
 } from '../../../redux/reducers/authSlice';
